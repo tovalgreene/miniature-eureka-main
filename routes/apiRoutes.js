03964953a54a8c66e384414955c3b5a39c4dd119
Tova Greene <tovalgreene@gmail.com>
2023-10-10 19:58:42 -0400

    edited apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,21 +3,23 @@ const router = express.Router();
 const fs = require('fs');
 const path = require('path');
 
+const dbFilePath = path.join(__dirname, '../db/db.json');
+
 // Read all notes
 router.get('/notes', (req, res) => {
-    const notes = JSON.parse(fs.readFileSync('../db/db.json', 'utf8'));
-    res.json(notes);
+  const notes = JSON.parse(fs.readFileSync(dbFilePath, 'utf8'));
+  res.json(notes);
 });
 
 // Create new note
 router.post('/notes', (req, res) => {
   const newNote = req.body;
-  const notes = JSON.parse(fs.readFileSync('../db/db.json', 'utf8'));
+  const notes = JSON.parse(fs.readFileSync(dbFilePath, 'utf8'));
 
   newNote.id = notes.length + 1;
 
   notes.push(newNote);
-  fs.writeFileSync('../db/db.json', JSON.stringify(notes));
+  fs.writeFileSync(dbFilePath, JSON.stringify(notes));
 
   res.json(newNote);
 });
