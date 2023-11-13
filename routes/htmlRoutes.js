2df36e2dfb3fc7c5cb596917f522900044a6487a
Tova Greene <tovalgreene@gmail.com>
2023-11-12 19:35:10 -0500

    edited htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,15 +1,15 @@
-const express = require('express');
 const path = require('path');
+const express = require('express');
 const router = express.Router();
 
-//Route to notes.html
-router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
+// Home page route
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
-//Route to index.html for all other routes
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+// Notes page route
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'notes.html'));
 });
 
 module.exports = router;
